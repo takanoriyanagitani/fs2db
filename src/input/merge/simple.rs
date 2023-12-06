3790bbffe4ff1ce5641737933ba7341a0f47f707
yanagitani <takanori.yanagitani@gmail.com>
2023-12-07 08:02:43 +0900

    docs: more docs

--- src/input/merge/simple.rs
+++ src/input/merge/simple.rs
@@ -94,6 +94,34 @@ where
 /// - sa: A source with fewer elements(its contents will be stored in a BTreeMap)
 /// - sb: A source which may contain many elements
 /// - merger: A merger which merges a value of sa and a value of sb
+///
+/// ## Example
+///
+/// ### Source A
+///
+/// | Key | Val                                                         |
+/// |:---:|:-----------------------------------------------------------:|
+/// | 1   | {"name": "plane 1", "width": "37.76mm", "height": "59.9mm"} |
+/// | 2   | {"name": "plane 2", "width": "63.40mm", "height": "33.3mm"} |
+/// | 3   | {"name": "plane 3", "width": "2.997mm", "height": "92.4mm"} |
+///
+/// ### Source B
+///
+/// | Key | Val                                                                   |
+/// |:---:|:---------------------------------------------------------------------:|
+/// | 1   | {"histid": 10, "price": 42.0, "timestamp": "2023-12-07T07:58:59.0Z" } |
+/// | 1   | {"histid": 11, "price": 43.0, "timestamp": "2023-12-08T07:58:59.0Z" } |
+/// | 2   | {"histid": 12, "price": 43.0, "timestamp": "2023-12-08T07:58:59.0Z" } |
+/// | 2   | {"histid": 13, "price": 43.0, "timestamp": "2023-12-08T07:58:59.0Z" } |
+///
+/// ### Source
+///
+/// | Key | Val                                                                             |
+/// |:---:|:-------------------------------------------------------------------------------:|
+/// | 1   | {"name": "plane 1", "width": "37.76mm", "height": "59.9mm", "histid": 10, ... } |
+/// | 1   | {"name": "plane 1", "width": "37.76mm", "height": "59.9mm", "histid": 11, ... } |
+/// | 2   | {"name": "plane 2", "width": "63.40mm", "height": "33.3mm", "histid": 12, ... } |
+/// | 2   | {"name": "plane 2", "width": "63.40mm", "height": "33.3mm", "histid": 13, ... } |
 pub fn source_new_merged<A, B, M>(sa: A, sb: B, merger: M) -> impl Source<Item = M::T>
 where
     A: Clone + MergeSource,
