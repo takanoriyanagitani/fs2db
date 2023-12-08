c816986b701eed251ff34a34dd0c5d13cceb1b06
yanagitani <takanori.yanagitani@gmail.com>
2023-12-08 15:14:27 +0900

    feat: a trait to get all key/val pairs in a bucket

--- src/input/source.rs
+++ src/input/source.rs
@@ -10,3 +10,14 @@ pub trait Source: Send + Sync + 'static {
 
     async fn all(&self) -> Result<Self::All, Status>;
 }
+
+#[tonic::async_trait]
+pub trait BucketSource: Send + Sync + 'static {
+    type Bucket;
+    type K;
+    type V;
+
+    type All: Stream<Item = Result<(Self::K, Self::V), Status>> + Send + 'static;
+
+    async fn get_all_by_bucket(&self, b: Self::Bucket) -> Result<Self::All, Status>;
+}
