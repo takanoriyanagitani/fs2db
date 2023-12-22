d84f7a9384d54d94cf4728fd7d5c45e77b282bb1
yanagitani <takanori.yanagitani@gmail.com>
2023-12-22 12:28:18 +0900

    feat: nearest impl for simple range

--- src/input/bin/mem/btree.rs
+++ src/input/bin/mem/btree.rs
@@ -1,3 +1,5 @@
+use core::ops::{RangeBounds, RangeInclusive};
+
 use std::collections::{BTreeMap, BTreeSet};
 use std::sync::Arc;
 
@@ -10,6 +12,146 @@ pub trait Nearest: Sync + Send + 'static {
 
     fn get_nearest(&self, k: &Self::K, m: &BTreeSet<Self::K>) -> Result<Self::K, Status>;
 }
+
+pub trait Range: Sync + Send + 'static {
+    type K: Sync + Send + Ord;
+    type R: RangeBounds<Self::K>;
+
+    fn range(&self, key: &Self::K) -> Self::R;
+}
+
+pub trait ComputeNearest: Sync + Send + 'static {
+    type K: Sync + Send + Ord;
+    type Score: Sync + Send + Ord;
+
+    fn nearest<I>(&self, pairs: I) -> Result<Self::K, Status>
+    where
+        I: Iterator<Item = (Self::Score, Self::K)>;
+}
+
+pub trait ComputeDiff: Sync + Send + 'static {
+    type K: Sync + Send + Ord;
+    type Score: Sync + Send + Ord;
+
+    fn compute_score(&self, a: &Self::K, b: &Self::K) -> Self::Score;
+}
+
+impl<T> Nearest for T
+where
+    T: Range,
+    <T as Range>::K: Clone,
+    T: ComputeNearest<K = <T as Range>::K>,
+    T: ComputeDiff<K = <T as Range>::K, Score = <T as ComputeNearest>::Score>,
+{
+    type K = <T as Range>::K;
+
+    fn get_nearest(&self, k: &Self::K, m: &BTreeSet<Self::K>) -> Result<Self::K, Status> {
+        let range: T::R = self.range(k);
+        let keys = m.range(range);
+        let pairs = keys.map(|key: &Self::K| (self.compute_score(k, key), key.clone()));
+        self.nearest(pairs)
+    }
+}
+
+pub struct RangeII<K> {
+    pub lbi_offset: K,
+    pub ubi_offset: K,
+}
+
+macro_rules! range_impl {
+    ($key: ty) => {
+        impl Range for RangeII<$key> {
+            type K = $key;
+            type R = RangeInclusive<Self::K>;
+
+            fn range(&self, key: &Self::K) -> Self::R {
+                let lbi: Self::K = key.saturating_sub(self.lbi_offset);
+                let ubi: Self::K = key.saturating_add(self.ubi_offset);
+                lbi..=ubi
+            }
+        }
+    };
+}
+
+macro_rules! range_impl_many {
+    ($($key: ty)*) => ($(
+        range_impl!($key);
+    )*)
+}
+
+range_impl_many!(u8 u16 u32 u64 u128 usize);
+range_impl_many!(i8 i16 i32 i64 i128 isize);
+
+macro_rules! cdiff_impl {
+    ($key: ty, $score: ty) => {
+        impl ComputeDiff for RangeII<$key> {
+            type K = $key;
+            type Score = $score;
+
+            fn compute_score(&self, a: &Self::K, b: &Self::K) -> Self::Score {
+                a.abs_diff(*b)
+            }
+        }
+    };
+}
+
+macro_rules! cdiff_impl_u_many {
+    ($($key: ty)*) => ($(
+        cdiff_impl!($key, $key);
+    )*)
+}
+
+cdiff_impl_u_many!(u8 u16 u32 u64 u128 usize);
+
+cdiff_impl!(i8, u8);
+cdiff_impl!(i16, u16);
+cdiff_impl!(i32, u32);
+cdiff_impl!(i64, u64);
+cdiff_impl!(i128, u128);
+cdiff_impl!(isize, usize);
+
+macro_rules! cnear_impl {
+    ($key: ty, $score: ty) => {
+        impl ComputeNearest for RangeII<$key> {
+            type K = $key;
+            type Score = $score;
+
+            fn nearest<I>(&self, pairs: I) -> Result<Self::K, Status>
+            where
+                I: Iterator<Item = (Self::Score, Self::K)>,
+            {
+                let min: Option<(Self::Score, Self::K)> = pairs.fold(None, |o, pair| match o {
+                    None => Some(pair),
+                    Some(old) => {
+                        let (oscore, okey) = old;
+                        let (score, key) = pair;
+                        match oscore < score {
+                            true => Some((oscore, okey)),
+                            false => Some((score, key)),
+                        }
+                    }
+                });
+                min.map(|p| p.1)
+                    .ok_or_else(|| Status::invalid_argument("empty pairs"))
+            }
+        }
+    };
+}
+
+macro_rules! cnear_impl_u_many {
+    ($($key: ty)*) => ($(
+        cnear_impl!($key, $key);
+    )*)
+}
+
+cnear_impl_u_many!(u8 u16 u32 u64 u128 usize);
+
+cnear_impl!(i8, u8);
+cnear_impl!(i16, u16);
+cnear_impl!(i32, u32);
+cnear_impl!(i64, u64);
+cnear_impl!(i128, u128);
+cnear_impl!(isize, usize);
 
 pub struct BinSource<M, N> {
     msrc: M,
@@ -174,6 +316,60 @@ mod test_btree {
                 assert_eq!(2, got.len());
                 assert_eq!(&599.0, got.get(&300).unwrap());
                 assert_eq!(&3776.0, got.get(&600).unwrap());
+            }
+        }
+    }
+
+    mod range_ii {
+        mod empty {
+            use std::collections::BTreeSet;
+
+            use crate::input::bin::mem::btree::{Nearest, RangeII};
+
+            #[tokio::test]
+            async fn ensure_empty() {
+                let r = RangeII {
+                    lbi_offset: 100,
+                    ubi_offset: 100,
+                };
+
+                let set: BTreeSet<i32> = BTreeSet::new();
+                let rslt = r.get_nearest(&0, &set);
+                assert_eq!(true, rslt.is_err());
+            }
+        }
+
+        mod simple {
+            use std::collections::BTreeSet;
+
+            use crate::input::bin::mem::btree::{Nearest, RangeII};
+
+            #[tokio::test]
+            async fn ensure_empty() {
+                let r = RangeII {
+                    lbi_offset: 100,
+                    ubi_offset: 100,
+                };
+
+                let set: BTreeSet<i32> = BTreeSet::from_iter(vec![99, 500]);
+
+                let got: i32 = r.get_nearest(&50, &set).unwrap();
+                assert_eq!(99, got);
+
+                let got: i32 = r.get_nearest(&150, &set).unwrap();
+                assert_eq!(99, got);
+
+                let got: i32 = r.get_nearest(&450, &set).unwrap();
+                assert_eq!(500, got);
+
+                let got: i32 = r.get_nearest(&550, &set).unwrap();
+                assert_eq!(500, got);
+
+                let rslt = r.get_nearest(&1000, &set);
+                assert_eq!(true, rslt.is_err());
+
+                let rslt = r.get_nearest(&-100, &set);
+                assert_eq!(true, rslt.is_err());
             }
         }
     }
